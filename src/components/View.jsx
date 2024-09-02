bd179b05ea52338089252fc452acc0fe91ac332f
rnunez <rnunez@york.cuny.edu>
2024-09-02 14:55:49 -0400

    button should not be clickable on edit and external links will open in new tab

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
 import cx from 'classnames';
-import { ConditionalLink } from '@plone/volto/components';
+import { Link } from 'react-router-dom';
 import { flattenToAppURL, isInternalURL } from '@plone/volto/helpers';
 import { defineMessages, useIntl } from 'react-intl';
 
@@ -13,51 +13,40 @@ const messages = defineMessages({
 });
 
 const View = ({ data, isEditMode, className }) => {
-  const [hasLink, setHasLink] = React.useState(false);
   const intl = useIntl();
 
-  React.useEffect(() => {
-    if (data.href) {
-      if (data.href && data.href.length > 0) {
-        setHasLink(true);
-      }
-      if (data.href.length === 0) {
-        setHasLink(false);
-      }
-    }
+  // Determine if there is a link
+  const hasLink = data.href && data.href.length > 0;
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [data.href]);
+  // Determine if the link is internal or external
+  const isInternal = hasLink && isInternalURL(data.href[0]['@id']);
 
-  let link = hasLink ? (
-    data.href.length > 0 && isInternalURL(data.href[0]['@id']) ? (
-      <ConditionalLink
-        to={data.href.length > 0 ? flattenToAppURL(data.href[0]['@id']) : ''}
-        condition={!isEditMode}
-      >
-        <Button className={(cx('button'), data.align, data.emphasis)} size={data.size}>
-          {data.title || intl.formatMessage(messages.ButtonText)}
-        </Button>
-      </ConditionalLink>
-    ) : (
-      data.href.length > 0 && (
-        <a href={flattenToAppURL(data.href[0]['@id'])}>
-          <Button className={(cx('button'), data.align, data.emphasis)} size={data.size}>
-            {data.title || intl.formatMessage(messages.ButtonText)}
-          </Button>
-        </a>
-      )
-    )
-  ) : (
-    <Button className="noLink">
+  // Flatten the URL if present
+  const url = hasLink ? flattenToAppURL(data.href[0]['@id']) : '';
+
+  // Determine the link element
+  const linkElement = isEditMode ? (
+    <Button className={cx('button', data.align, data.emphasis)} size={data.size}>
       {data.title || intl.formatMessage(messages.ButtonText)}
     </Button>
+  ) : isInternal ? (
+    <Link to={url} target='_self'>
+      <Button className={cx('button', data.align, data.emphasis)} size={data.size}>
+        {data.title || intl.formatMessage(messages.ButtonText)}
+      </Button>
+    </Link>
+  ) : (
+    <a href={url} target='_blank' rel='noopener noreferrer'>
+      <Button className={cx('button', data.align, data.emphasis)} size={data.size}>
+        {data.title || intl.formatMessage(messages.ButtonText)}
+      </Button>
+    </a>
   );
 
   return (
     <div className={cx('block __button', className)}>
       <div className="button container">
-        <div className={cx(`align ${data?.inneralign}`)}>{link}</div>
+        <div className={cx(`align ${data?.inneralign}`)}>{linkElement}</div>
       </div>
     </div>
   );
